83e35f545e85163a035f0cebe193b1eb23ab13c9
Mario-Lapi <145753293+Mario-Lapi@users.noreply.github.com>
2024-02-22 12:56:57 +0100

    premier code qui genere des grilles de picross

---
+++ code_picross/app.R
@@ -0,0 +1,82 @@
+library(shiny)
+
+# Define UI for application that draws a grid of black and white squares
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Grille de PICROSS"),
+  
+  # Use fixed layout
+  tags$head(tags$style(HTML("
+        body, .container-fluid {
+            max-width: 800px; /* adjust this value to your desired maximum width */
+            width: 800px !important;
+        }
+    "))),
+  
+  # Sidebar with a select input for grid size
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("grid_size",
+                  "Grid Size:",
+                  choices = c("5x5" = "5x5",
+                              "5x10" = "5x10",
+                              "10x10" = "10x10",
+                              "10x15" = "10x15",
+                              "15x15" = "15x15",
+                              "15x20" = "15x20",
+                              "20x20" = "20x20"),
+                  selected = "10x10")
+    ),
+    
+    # Show the grid of black and white squares
+    mainPanel(
+      plotOutput("gridPlot", height = "600px")  # Adjust height to make the grid bigger
+    )
+  )
+)
+
+# Define server logic required to draw the grid
+server <- function(input, output) {
+  
+  output$gridPlot <- renderPlot({
+    # Extract grid dimensions from input$grid_size
+    dim <- as.numeric(unlist(strsplit(input$grid_size, "x")))
+    
+    # Generate grid based on dimensions
+    grid <- matrix(sample(c(0, 1), dim[1] * dim[2], replace = TRUE), nrow = dim[1], ncol = dim[2])
+    
+    # Calculate number of blue squares in each row and column
+    row_sums <- rowSums(grid)
+    col_sums <- colSums(grid)
+    
+    # Set up plot area
+    plot_range <- max(dim) + 1
+    par(mar = c(3, 3, 1, 1))
+    plot(0, 0, xlim = c(-0.5, plot_range), ylim = c(-0.5, plot_range), type = "n", xlab = "", ylab = "", xaxt = "n", yaxt = "n")
+    
+    # Plot the grid of black and white squares
+    for (i in 1:dim[1]) {
+      for (j in 1:dim[2]) {
+        if (grid[i, j] == 1) {
+          rect(j - 1, dim[1] - i, j, dim[1] - i + 1, col = "darkblue", border = "black")
+        } else {
+          rect(j - 1, dim[1] - i, j, dim[1] - i + 1, col = "white", border = "black")
+        }
+      }
+    }
+    
+    # Add labels for row sums
+    for (i in 1:dim[1]) {
+      text(-0.5, dim[1] - i + 0.5, row_sums[i], pos = 4)
+    }
+    
+    # Add labels for column sums
+    for (j in 1:dim[2]) {
+      text(j - 0.5, dim[1] + 0.5, col_sums[j], pos = 3)
+    }
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
