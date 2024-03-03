4d62a0eb9342f1b713318f3136606ff91f4e4043
Mario-Lapi <145753293+Mario-Lapi@users.noreply.github.com>
2024-03-03 16:19:15 +0100

    Option pour la taille de la grille

--- code_picross/matrice_boutons.R
+++ code_picross/matrice_boutons.R
@@ -1,28 +1,47 @@
 library(shiny)
 
 ui <- fluidPage(
-  titlePanel("Matrice de Boutons Carrés"),
-  uiOutput("buttonMatrix"),
-  tags$head(
-    tags$style(HTML("
-      .square-button {
-        width: 50px;
-        height: 50px;
-        margin: 2px;
-      }
-    "))
+  titlePanel("Grille de PICROSS"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("grid_size",
+                  "Grid Size:",
+                  choices = c("5x5" = "5x5",
+                              "5x10" = "5x10",
+                              "10x10" = "10x10",
+                              "10x15" = "10x15",
+                              "15x15" = "15x15",
+                              "15x20" = "15x20",
+                              "20x20" = "20x20"),
+                  selected = "5x5")
+    ),
+    mainPanel(
+      uiOutput("buttonMatrix"),
+      tags$head(
+        tags$style(HTML("
+          .square-button {
+            width: 50px;
+            height: 50px;
+            margin: 1px;
+          }
+        "))
+      )
+    )
   )
 )
 
 server <- function(input, output) {
   output$buttonMatrix <- renderUI({
-    buttons <- lapply(1:25, function(i) {
+    grid_size <- as.numeric(unlist(strsplit(input$grid_size, "x")))
+    num_buttons <- grid_size[1] * grid_size[2]
+    
+    buttons <- lapply(1:num_buttons, function(i) {
       actionButton(inputId = paste0("button", i), "", class = "btn btn-default square-button")
     })
     
-    matrix_buttons <- matrix(buttons, nrow = 5, byrow = TRUE)
+    matrix_buttons <- matrix(buttons, nrow = grid_size[1], byrow = TRUE)
     
-    rows <- lapply(1:5, function(i) {
+    rows <- lapply(1:grid_size[1], function(i) {
       fluidRow(do.call(tagList, matrix_buttons[i, ]))
     })
     
@@ -31,4 +50,3 @@ server <- function(input, output) {
 }
 
 shinyApp(ui, server)
-# 
